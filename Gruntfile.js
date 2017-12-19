5792bfd06d0d234c5064de77a127d62581934568
Slade Utley <SladeUtley@Slades-MacBook-Pro.local>
2017-12-19 10:41:02 -0600

    set up grunt

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        browserify: {
+            'dist/bundle.js': ['javascripts/main.js']
+        },
+        jshint: {
+            files: ['javascripts/**/*.js'],
+            options: {
+                predef: ["document", "console"],
+                esnext: true,
+                globalstrict: true,
+                globals: {},
+                browserify: true
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    'stylesheets/main.css': 'sass/main.scss'
+                }
+            }
+        },
+        watch: {
+            javascripts: {
+                files: ['javascripts/**/*.js'],
+                tasks: ['jshint', 'browserify']
+            },
+            sass: {
+                files: ["sass/**/*.scss"],
+                tasks: ["sass"]
+            }
+        }
+
+    });
+
+    require("matchdep")
+        .filter("grunt-*")
+        .forEach(grunt.loadNpmTasks);
+
+    grunt.registerTask("default", ['jshint', 'sass', 'browserify', 'watch']);
+};
